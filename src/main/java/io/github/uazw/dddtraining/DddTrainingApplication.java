50e9cea3d75e9ea5bf215a193b02a89249a1781a
yangwen <yangwen@thoughtworks.ocm>
2018-04-06 23:28:15 +0800

    project init

---
+++ src/main/java/io/github/uazw/dddtraining/DddTrainingApplication.java
@@ -0,0 +1,12 @@
+package io.github.uazw.dddtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddTrainingApplication.class, args);
+	}
+}
---
+++ src/test/java/io/github/uazw/dddtraining/DddTrainingApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.uazw.dddtraining;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DddTrainingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
